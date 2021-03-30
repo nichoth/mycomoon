8cdf7e9edb113409adedfe2fd2fe16a8098b89a8
nichoth <nichoth@gmail.com>
2021-03-29 20:48:50 -0700

    works i guess

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 
 [![Netlify Status](https://api.netlify.com/api/v1/badges/ec517db2-97d2-48ac-803a-c21453d83cea/deploy-status)](https://app.netlify.com/sites/musing-kirch-9f2f0c/deploys)
 
-https://medium.com/samsung-internet-dev/pwa-series-service-workers-the-basics-about-offline-a6e8f1d92dfd
+* https://medium.com/samsung-internet-dev/pwa-series-service-workers-the-basics-about-offline-a6e8f1d92dfd
+* https://www.smashingmagazine.com/2016/02/making-a-service-worker/
 
 A website
--- src/sw.js
+++ src/sw.js
@@ -5,6 +5,7 @@
 // * check that resources are up-to-date
 
 // https://medium.com/samsung-internet-dev/pwa-series-service-workers-the-basics-about-offline-a6e8f1d92dfd
+// https://www.smashingmagazine.com/2016/02/making-a-service-worker/
 
 // Perform install steps
 let CACHE_NAME = 'my-cache'
@@ -29,7 +30,7 @@ let urlsToCache = [
 ]
 
 self.addEventListener('install', function (ev) {
-    // Perform install steps
+    // Perform install steps -- cache all our urls right away
     console.log('**install event**', ev)
 
     ev.waitUntil(caches.open(CACHE_NAME)
@@ -43,23 +44,121 @@ self.addEventListener('install', function (ev) {
 // This works great for static content but if we have dynamic content,
 // it will never be updated.
 self.addEventListener('fetch', function (ev) {
-    ev.respondWith(caches.match(ev.request)
-            .then(function (response) {
-                console.log('cache hit', response)
-                // Cache hit - return response
-                if (response) return response
-                console.log('ev.requst', ev.request)
-                return fetch(ev.request)
-            })
-            .catch(err => console.log('!!!!err!! here', err))
-    )
+
+    // -------------------------------------------------------------
+
+    // below is based on the content type --
+    // using fetch-first for content, cache for static
+
+    var { request } = ev
+    var acceptHeader = request.headers.get('Accept')
+    console.log('acceptHeader', acceptHeader)
+    var resourceType = 'static'
+    var cacheKey
+
+    if ((acceptHeader || []).indexOf('text/html') !== -1) {
+        resourceType = 'content';
+    } else if ((acceptHeader || []).indexOf('image') !== -1) {
+        resourceType = 'image';
+    }
+
+    cacheKey = resourceType
+
+    // 1. Determine what kind of asset this is… (above).
+    if (resourceType === 'content') {
+        // Use a network-first strategy.
+        ev.respondWith(
+            fetch(request)
+            .then(response => addToCache(cacheKey, request, response))
+            .catch(() => fetchFromCache(ev))
+            // .catch(() => offlineResponse(opts))
+        );
+    } else {
+        // Use a cache-first strategy.
+        ev.respondWith(
+            fetchFromCache(ev)
+            .catch(() => fetch(request))
+            .then(response => addToCache(cacheKey, request, response))
+            // .catch(() => offlineResponse(resourceType, opts))
+        )
+    }
+
+
+
+
+
+
+
+    // ev.respondWith(caches.match(ev.request)
+    //     .then(function (response) {
+    //         // using a cache-first strategy
+    //         console.log('cache hit', response)
+    //         // Cache hit -- return response
+    //         if (response) return response
+
+    //         // cache miss -- request it
+    //         console.log('ev.requst', request)
+    //         return fetch(request)
+
+
+
+
+
+
+
+    //     })
+    //     .catch(err => console.log('!!!!err!! here', err))
+    // )
 })
+
+
+
+function addToCache (cacheKey, request, response) {
+    if (response.ok) {
+        var copy = response.clone()
+        caches.open(cacheKey).then(cache => {
+            cache.put(request, copy)
+        })
+
+        return response
+    }
+}
+
+
+function fetchFromCache (ev) {
+    return caches.match(ev.request).then(response => {
+        if (!response) {
+            // A synchronous error that will kick off the catch handler
+            throw Error(`${ev.request.url} not found in cache`)
+        }
+        return response
+    })
+}
+
+
+
+
+// function shouldHandleFetch (ev, opts) {
+//     // Should we handle this fetch?
+//     var request = ev.request
+//     var url = new URL(request.url)
+//     console.log('path', url.pathname)
+
+//     return ev.respondWith(caches.match(ev.request))
+// }
+
+// function onFetch (ev, opts) {
+//     // … TBD: Respond to the fetch
+// }
+
+
+
 
 // the browser will check if these resources are in the previous cache
 // list. if they don’t exist anymore it will remove them.
 self.addEventListener('activate', function (ev) {
     console.log('**activate', ev)
-    var cacheWhitelist = ['my-cache']
+    var cacheWhitelist = ['my-cache', 'static']
 
     ev.waitUntil(
         caches.keys().then(function (cacheNames) {
