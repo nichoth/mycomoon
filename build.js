58ff7f11d50fa728704ed2aefe84b40b2e7961c2
nichoth <nichoth@gmail.com>
2021-04-15 11:55:14 -0700

    build script for pages

---
+++ build.js
@@ -0,0 +1,35 @@
+var fs = require('fs')
+var path = require('path')
+var mkdirp = require('mkdirp')
+const matter = require('gray-matter')
+var hyperstream = require('hyperstream')
+
+fs.readdir(__dirname + '/src/_pages', (err, files) => {
+    if (err) throw err
+
+    files.forEach(fileName => {
+        var name = path.basename(fileName, '.md')
+        var dirPath = path.join(__dirname + '/public', name)
+
+
+
+        mkdirp(dirPath).then(() => {
+            console.log('made dir', dirPath)
+            // then write the index file
+            var srcPath = __dirname + '/src/_pages/' + fileName 
+            fs.readFile(srcPath, (err, content) => {
+                if (err) throw err
+
+                var hs = hyperstream({
+                    '#content': {
+                        _appendHtml: matter(content).content
+                    }
+                })
+
+                var rs = fs.createReadStream(__dirname + '/src/_index.html');
+                var ws = fs.createWriteStream(dirPath + '/index.html')
+                rs.pipe(hs).pipe(ws)
+            })
+        })
+    })
+})
