e138497c4b78e90a9ca472eb32852198dc9a8ca1
nichoth <nichoth@gmail.com>
2021-04-24 10:44:07 -0700

    fix indentation

--- netlify/functions/hello-world/hello-world.js
+++ netlify/functions/hello-world/hello-world.js
@@ -1,31 +1,31 @@
 const { Client, Environment } = require('square')
 
 const client = new Client({
-  environment: Environment.Sandbox,
-  accessToken: process.env.SQUARE_ACCESS_TOKEN,
+    environment: Environment.Sandbox,
+    accessToken: process.env.SQUARE_ACCESS_TOKEN,
 })
 
 // Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
 const handler = async (event) => {
 
-  console.log('envvvv', process.env.SQ_SANDBOX_APP_ID)
-  // console.log('client', client)
+    console.log('envvvv', process.env.SQ_SANDBOX_APP_ID)
+    // console.log('client', client)
 
-  try {
-    const subject = event.queryStringParameters.name || 'World'
-    return {
-      statusCode: 200,
-      body: JSON.stringify({
-        message: `Hello ${subject}`,
-        appId: process.env.SQ_SANDBOX_APP_ID
-      }),
-      // // more keys you can return:
-      // headers: { "headerName": "headerValue", ... },
-      // isBase64Encoded: true,
+    try {
+        const subject = event.queryStringParameters.name || 'World'
+        return {
+            statusCode: 200,
+            body: JSON.stringify({
+                message: `Hello ${subject}`,
+                appId: process.env.SQ_SANDBOX_APP_ID
+            }),
+            // // more keys you can return:
+            // headers: { "headerName": "headerValue", ... },
+            // isBase64Encoded: true,
+        }
+    } catch (error) {
+        return { statusCode: 500, body: error.toString() }
     }
-  } catch (error) {
-    return { statusCode: 500, body: error.toString() }
-  }
 }
 
 module.exports = { handler }
