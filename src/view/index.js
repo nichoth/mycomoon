2efa237299d585bf2fb4848e7a10767f5fa4f626
nichoth <nichoth@gmail.com>
2021-05-12 11:59:39 -0700

    show products by default

--- src/view/index.js
+++ src/view/index.js
@@ -6,18 +6,20 @@ function IndexView (props) {
     // we're doing it this weird way because it doesn't work if you use a HOC
     var { getContent, path } = props
 
+    console.log('props', props)
+
     return html`
         <ul class="main-nav">
-            <li class="tab${path.includes('about') ? ' active' : ''}">
-                <a href="/about"><h2>about</h2></a>
-                <div class="page-content">the about content</div>
-            </li>
-
-            <li class="tab${path.includes('products') ? ' active' : ''}">
-                <a href="/products"><h2>products</h2></a>
+            <li class="tab${path === '/' ? ' active' : ''}">
+                <a href="/"><h2>products</h2></a>
                 <div class="page-content">
                     <${Products} getContent=${getContent} />
                 </div>
+            </li>
+
+            <li class="tab${path.includes('about') ? ' active' : ''}">
+                <a href="/about"><h2>about</h2></a>
+                <div class="page-content">the about content</div>
             </li>
         </ul>
     `
