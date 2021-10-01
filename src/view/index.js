0af216b858f5d7546e3ec0208309613286033c3b
nichoth <nichoth@gmail.com>
2021-10-01 10:37:05 -0700

    wip redesign

--- src/view/index.js
+++ src/view/index.js
@@ -12,6 +12,14 @@ function IndexView (props) {
         <div class="logo">
             <img src="/img/logo.png" />
         </div>
+
+        <p class="home-text">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            Ut enim ad minim veniam, quis nostrud exercitation ullamco
+            laboris nisi ut aliquip ex ea commodo consequat.
+            Mollit anim id est laborum.
+        </p>
     `
 }
 
