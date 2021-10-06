d7f8fe1bcd162bf4a49250b0701dcdb3d0b79efd
nichoth <nichoth@gmail.com>
2021-10-06 16:26:40 -0700

    better list style

--- src/CONSTANTS.js
+++ src/CONSTANTS.js
@@ -1,7 +1,8 @@
 var ITEMS = [
     { link: 'turkey-tail-tincture', name: 'Turkey Tail Tincture' },
     { link: 'chaga-tincture', name: 'Chaga Tincture' },
-    // { link: 'reishi-tincture', name: "Reishi Tincture" }
+    { link: 'reishi-tincture', name: "Reishi Tincture" },
+    { link: 'lions-mane', name: "Lion's Mane Tincture"}
 ]
 
 module.exports = {
